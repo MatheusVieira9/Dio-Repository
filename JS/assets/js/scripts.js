c49adc477579f90a08890991988b06f88651b1cc
Matheus v. costa <matheusv99@outlook.com>
2022-06-13 15:06:03 -0300

    programando um contador no JS

--- JS/assets/js/scripts.js
+++ JS/assets/js/scripts.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+const currentNumberwrapper = document.getElementById('currentNumber');
+let cout = 0;
+
+function increment() {
+    cout++;
+    currentNumberwrapper.innerHTML = cout;
+}
+
+function decrement() {
+    cout--;
+    currentNumberwrapper.innerHTML = cout;
+}
